eb9dfbdbf0665041be5f905170e13ab685a2464b
Noot <ldaniel358@gmail.com>
2023-05-09 12:37:59 -0400

    feat: still experimenting with achitectures

--- src/app.py
+++ src/app.py
@@ -1,22 +1,14 @@
-import os
 from flask import Flask
 from flask_restful import Api
 from resources.games_list import GamesList
-from resources.game_moves import GameMoves
+from resources.tictactoe import TicTacToe
 
 app = Flask(__name__)
-
-if os.environ.get("FLASK_ENV") == "development":
-    app.debug = True
-else:
-    app.debug = False
-
-
 api = Api(app)
 
 api.add_resource(GamesList, "/games")
-api.add_resource(GameMoves, "/games/<string:game_id>/moves")
+api.add_resource(TicTacToe, "/tictactoe/move", "/tictactoe/moves")
 
 
 if __name__ == "__main__":
-    app.run(debug=app.debug)
+    app.run(debug=True)
